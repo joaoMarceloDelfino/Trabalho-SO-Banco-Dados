9f53c4af8e45179d193c3ed5b59c47ef630ad4ab
Lipe-meira <felipemeira018@gmail.com>
2026-04-02 11:02:01 -0300

    feat: implementado threads nativas do Windows e exclusao mutua para processamento paralelo (threads e mutex)

--- servidor.cpp
+++ servidor.cpp
@@ -4,12 +4,17 @@
 #include <cstring>
 #include <sstream>
 #include <windows.h> // IPC do Windows
-
+//tava dando problema com o Mingw, pelo que pesquisei eh bug do windows....
+// #include <thread> 
+// #include <mutex>  
 
 #define PIPE_NAME "\\\\.\\pipe\\banco_pipe"
 
 Registro tabela[MAX_REGISTROS];
 int totalRegistros = 0;
+
+// Mutex nativo do Windows (Equivalente ao pthread_mutex_t)
+CRITICAL_SECTION mutex_banco;
 
 void salvarBancoJson() {
     std::ofstream arquivo("banco.json");
@@ -106,10 +111,18 @@ bool deletarRegistro(int id) {
     return false;
 }
 
-void processarRequisicao(const std::string& comando) {
+// Formato exigido para a thread nativa do Windows (DWORD WINAPI)
+DWORD WINAPI processarRequisicao(LPVOID arg) {
+    char* comandoStr = (char*)arg;
+    std::string comando(comandoStr);
+    delete[] comandoStr; // Libera a memória da string
+
     std::istringstream iss(comando);
     std::string acao;
     iss >> acao;
+
+    // TRAVA O MUTEX
+    EnterCriticalSection(&mutex_banco);
 
     if (acao == "INSERT" || acao == "UPDATE") {
         int id; std::string tipoStr;
@@ -127,22 +140,32 @@ void processarRequisicao(const std::string& comando) {
 
         if (acao == "INSERT") inserirRegistro(id, v, tipo);
         else atualizarRegistro(id, v, tipo);
-        std::cout << "[SERVIDOR] Processou sequencialmente: " << comando << "\n";
+        
+        // GetCurrentThreadId() pega o ID da thread atual no Windows
+        std::cout << "[THREAD " << GetCurrentThreadId() << "] Resolvido: " << comando << "\n";
     } 
     else if (acao == "DELETE") {
         int id; iss >> id;
         deletarRegistro(id);
-        std::cout << "[SERVIDOR] Processou sequencialmente: " << comando << "\n";
+        std::cout << "[THREAD " << GetCurrentThreadId() << "] Resolvido: " << comando << "\n";
     }
     else if (acao == "SELECT") {
         int id; iss >> id; Registro r;
-        if (buscarRegistro(id, &r)) std::cout << "[SERVIDOR] Busca resolvida: ID " << id << "\n";
+        if (buscarRegistro(id, &r)) std::cout << "[THREAD " << GetCurrentThreadId() << "] Busca resolvida: ID " << id << "\n";
     }
+    
+    // DESTRAVA O MUTEX
+    LeaveCriticalSection(&mutex_banco);
+
+    return 0;
 }
 
 int main() {
     inicializarBanco();
-    std::cout << "[SERVIDOR] Aguardando req pipe...\n";
+    // Inicia a variável do Mutex do Windows
+    InitializeCriticalSection(&mutex_banco);
+
+    std::cout << "[SERVIDOR] Aguardando req pipe e thread...\n";
 
     HANDLE hPipe = CreateNamedPipeA(PIPE_NAME, PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT, PIPE_UNLIMITED_INSTANCES, 512, 512, 0, NULL);
     if (hPipe == INVALID_HANDLE_VALUE) return 1;
@@ -153,11 +176,21 @@ int main() {
             char buffer[256]; DWORD bytesLidos;
             while (ReadFile(hPipe, buffer, sizeof(buffer) - 1, &bytesLidos, NULL) && bytesLidos != 0) {
                 buffer[bytesLidos] = '\0'; 
-                processarRequisicao(std::string(buffer)); // Chamada sequencial
+                
+                char* req = new char[strlen(buffer) + 1];
+                strcpy(req, buffer);
+
+           HANDLE hThread = CreateThread(NULL, 0, processarRequisicao, req, 0, NULL);
+                
+                // Fecha o handle da thread (equivalente ao detach, ela roda livre)
+                CloseHandle(hThread);
             }
         }
         DisconnectNamedPipe(hPipe);
     }
+    
+    // Limpa o mutex ao sair
+    DeleteCriticalSection(&mutex_banco);
     CloseHandle(hPipe);
     return 0;
 }
